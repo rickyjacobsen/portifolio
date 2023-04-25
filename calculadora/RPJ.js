ab31fedfdb96f1195daccb1c55e298542cb6a7d6
rickyjacobsen <rickyjacobsen@gmail.com>
2023-04-25 15:07:13 -0300

    sem focus

--- calculadora/RPJ.js
+++ calculadora/RPJ.js
@@ -5,7 +5,6 @@ var lista = []
 
 function limpar(){
     vi.value = "" 
-    vi.focus()
     lista.length = 0
     
 }
@@ -27,12 +26,11 @@ function dividir(){
         var total = lista[0] / lista [3]
         vi.value = total
         lista.length = 0
-        vi.focus()
+        
     }
     else{
         vi.value = ''
         lista.push(Number(vi.value)) 
-        vi.focus()
     }
 
 }
@@ -40,19 +38,16 @@ function dividir(){
 function sete(){
     var visor = vi.value
     vi.value = visor + 7
-    vi.focus()
 }
 
 function oito(){
     var visor = vi.value
     vi.value = visor +  8
-    vi.focus()
 }
 
 function nove(){
     var visor = vi.value
     vi.value = visor + 9
-    vi.focus()
 }
 
 function vezes(){
@@ -64,31 +59,26 @@ function vezes(){
         var total = lista[0] * lista [3]
         vi.value = total
         lista.length = 0
-        vi.focus()
     }
     else{
         vi.value = ''
         lista.push(Number(vi.value)) 
-        vi.focus()
     }
 }
 
 function quatro(){
     var visor = vi.value
     vi.value = visor + 4
-    vi.focus()
 }
 
 function cinco(){
     var visor = vi.value
     vi.value = visor +  5
-    vi.focus()
 }
 
 function seis(){
     var visor = vi.value
     vi.value = visor + 6
-    vi.focus()
 }
 
 function menos(){
@@ -100,31 +90,26 @@ function menos(){
         var total = lista[0] - lista [3]
         vi.value = total
         lista.length = 0
-        vi.focus()
     }
     else{
         vi.value = ''
         lista.push(Number(vi.value)) 
-        vi.focus()
     }
 }
 
 function um(){
     var visor = vi.value
     vi.value = visor +  1
-    vi.focus()
 }
 
 function dois(){
     var visor = vi.value
     vi.value = visor +  2
-    vi.focus()
 }
 
 function tres(){
     var visor = vi.value
     vi.value = visor +  3
-    vi.focus()
 }
 
 function mais(){
@@ -136,25 +121,21 @@ function mais(){
         var total = lista[0] + lista [3]
         vi.value = total
         lista.length = 0
-        vi.focus()
     }
     else{
         vi.value = ''
         lista.push(Number(vi.value)) 
-        vi.focus()
     }
 }
 
 function zero(){
     var visor = vi.value
     vi.value = visor + 0
-    vi.focus()
 }
 
 function virgula(){
     var visor = vi.value
     vi.value = visor + '.'
-    vi.focus()
 }
 
 function igual(){
@@ -163,28 +144,24 @@ function igual(){
         lista.push(Number(vi.value)) 
         vi.value = lista[0] / lista[3]
         lista.length = 0
-        vi.focus()
      }
 
      if (lista[1] == 'x'){
         lista.push(Number(vi.value)) 
         vi.value = lista[0] * lista[3]
         lista.length = 0
-        vi.focus()
      }
      
      if (lista[1] == '-'){
         lista.push(Number(vi.value)) 
         vi.value = lista[0] - lista[3]
         lista.length = 0
-        vi.focus()
      }
     
      if (lista[1] == '+'){
         lista.push(Number(vi.value)) 
         vi.value = lista[0] + lista[3]
         lista.length = 0
-        vi.focus()
      }
     
 }
