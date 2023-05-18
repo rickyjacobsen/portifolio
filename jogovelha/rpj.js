7d531d07ae095ff6c4d22bd90b3ba0845f9dc95c
rickyjacobsen <rickyjacobsen@gmail.com>
2023-05-17 23:43:57 -0300

    jogo da velha

---
+++ jogovelha/rpj.js
@@ -0,0 +1,12 @@
+
+
+let a1 = document.getElementById('a1')
+let a2 = document.getElementById('a2')
+let a3 = document.getElementById('a3')
+let b1 = document.getElementById('b1')
+let b2 = document.getElementById('b2')
+let b3 = document.getElementById('b3')
+let c1 = document.getElementById('c1')
+let c2 = document.getElementById('c2')
+let c3 = document.getElementById('c3')
+
